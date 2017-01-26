4a2f3839c8a079e836fa015c75bbd49fb7803709
vincentindino <vincentindino26@yahoo.com>
2017-01-26 14:05:18 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This website has a side menu bar</title>
+</head>
+<body bgcol="#99999">
+<div align="center">
+<table bgcolor="#FFFFFF" width="900" cellspacing="0" cellpadding="0" style="padding-top: 10px; padding-bottom: 10px;">
+
+
+<tr>
+<td align="center">
+<font face="arial" size="10">
+Website Banner Goes here!
+</font>
+</td>
+</tr>
+</table>
+
+<table bgcolor="#FFFFFF" width="900" cellspacing="0" cellpadding="0" style="padding-top: 10px; padding-bottom: 10px;">
+<tr>
+<td valign="top" width="250">
+<font face="arial" color="#FFFFFF" size="10">
+Home
+<br>
+About
+<br>
+Games
+<br>
+Links
+<br>
+Contacts
+<br>
+</font>
+</td>
+<td width="1000" valign="top">
+<font face="arial" size="10" color="#FFFFFF">
+Welcome To My Site, Here is my content!
+</font>
+</td>
+</tr>
+</table>
+
+
+
+
+
+
+<h1> My Favorites </h1>
+<h1> Eat </h1>
+<h1> Sleep </h1>
+<h1> Dota </h1>
+</body>
+</html>
